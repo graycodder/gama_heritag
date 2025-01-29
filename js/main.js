ac6f73ed6a28348faa54e94652508e5c83c5c795
Alwin Antony <alwin1658@gmail.com>
2025-01-29 17:35:19 +0530

    Cookies Added

--- js/main.js
+++ js/main.js
@@ -107,3 +107,18 @@
     $("select").niceSelect();
 
 })(jQuery);
+// Cookies Section Start//
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 2000);
+// Cookies Section End//
